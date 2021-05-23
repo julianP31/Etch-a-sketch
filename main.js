6f7aaa3e1a1dbb39c4b82223871423e010121c33
julianP31 <julian1percara2@gmail.com>
2021-05-23 07:33:38 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let $container = document.querySelector("#container")
 
-createSquare(30)
-modifyCss(30)
+defaultSize()
+
+function defaultSize() {
+    createSquare(16)
+    modifyCss(16)
+}
+
 function createSquare(size) {
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement("div")
@@ -15,12 +20,42 @@ function modifyCss(size) {
     $container.style = `grid-template-columns:repeat(${size},1fr); grid-template-rows:repeat(${size},1fr)`
 }
 function randomColor(e) {
-    
+
 }
 function mouseOver(e) {
     let r = Math.floor(Math.random() * 256)
     let g = Math.floor(Math.random() * 256)
     let b = Math.floor(Math.random() * 256)
     e.target.style = `background-color:rgb(${r},${g},${b})`;
+
+}
+
+let $buttonSize = document.querySelector("#btn-change-size").addEventListener("click",function (e){
+    changeSize()
+})
+
+function changeSize() {
+    let size =prompt("change the size of the grid")
     
+    if (size !== null){
+         size = parseInt(size)
+         if (size < 1 || size >100 || Number.isNaN(size)){
+             alert("choose the size of the grid (only numbers from 1 to 100)")
+             changeSize()
+         }
+         else{
+            removeOldDivs()
+            createSquare(size)
+            modifyCss(size)
+         }
+    }
+   
+   
+}
+
+function removeOldDivs() {
+    let $square = document.querySelectorAll(".square")
+    for (let i = 0; i < $square.length; i++) {
+        $square[i].remove()
+    }
 }
